a7831dbdbe78427166d4a63d1fa5a04ecc49994f
Chase Meadors <c.ed.mead@gmail.com>
2019-01-16 21:27:27 -0600

    Add fontawesome

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import './styles/theme.scss';
 import Vue from 'vue';
 import App from './components/App.vue';
 
-navigator.serviceWorker.register('/service-worker.js');
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCheckSquare, faSquare } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faCheckSquare);
+library.add(faSquare);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
     el: '#app',
